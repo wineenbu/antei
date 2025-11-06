6599211fe6f62c4d38f86a947880f87c8f5dfae7
yuni <ishiharakentamail@gmail.com>
2025-11-06 10:18:05 +0900

    3ff12gfq2q

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import json
 import datetime
 import asyncio
 import discord
-from timer import *  # timer.py ファイルのコマンドをインポート
+from Timer import *  # timer.py ファイルのコマンドをインポート
 from discord.ext import tasks
 from flask import Flask
 
